7e43cd9c2f5ed2b48f2116be82993243f7219717
Gabriel <gabrieldsr23@gmail.com>
2026-01-19 02:28:14 -0300

    Changed from APIView to ViewSet, and add search filter

--- articles/urls.py
+++ articles/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from articles import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register('article', views.ArticleViewSet)
 
 urlpatterns = [
-    path("articles/", views.ArticleList.as_view()),
-    path("articles/<int:pk>/", views.ArticleDetail.as_view()),
-]
+    path('', include(router.urls)),
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+]
